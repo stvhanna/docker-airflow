9d034940543447ab2835f5e2a1d78e63cef3097e
schnie <greg@differential.com>
2016-08-23 20:36:25 -0400

    Add logging to executors sync method

--- airflow/plugins/astronomer_plugin.py
+++ airflow/plugins/astronomer_plugin.py
@@ -300,8 +300,11 @@ class AstronomerMesosExecutor(BaseExecutor):
         self.task_queue.put((key, command))
 
     def sync(self):
+        logging.debug('calling astronomer executor sync method')
         while not self.result_queue.empty():
+            logging.debug('result_queue not empty, results:')
             results = self.result_queue.get()
+            logging.debug(results)
             self.change_state(*results)
 
     def end(self):
