e124e7b872f0c5b6f0583494e3e80eed8eb20bf1
Taylor Edmiston <tedmiston@gmail.com>
2016-11-17 04:20:41 -0500

    Fix timedelta import

--- astro_airflow/dags/ftp_dags.py
+++ astro_airflow/dags/ftp_dags.py
@@ -28,7 +28,7 @@ default_args = {
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'retry_delay': datetime.timedelta(minutes=5),
 }
 
 client = pymongo.MongoClient(MONGO_URL)
